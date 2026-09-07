46f429c779e72f45d2c6e6794722f695a671ae2b
ankitnkumar5 <anxkit5@gmail.com>
2026-09-07 09:50:59 +0530

    created array file and also written a example code

---
+++ Python/array.py
@@ -0,0 +1,12 @@
+# Create an array (list) of numbers
+numbers = [10, 20, 30, 40, 50]
+
+# Access an element by index
+print("First element:", numbers[0])
+
+# Add a new element to the end
+numbers.append(60)
+
+# Iterate through the array
+for num in numbers:
+    print(num)
